13eece3649bdb72361f26d3c58c198e741ab6f17
Seb Gertz <sebgertz@gmail.com>
2018-05-07 22:41:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function  updateDriverWithKeyAndValue(driver, key, value) {
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
-  return driver
+  return driver;
 }
 
 function deleteFromDriverByKey(driver, key) {
